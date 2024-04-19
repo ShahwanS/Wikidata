89f2cf7a3053b28a7439db27c01515ee27ea83f6
Lukas Kasimir Vogt <vogtlu@students.uni-marburg.de>
2024-04-19 17:06:28 +0200

    Add JSdoc-conventions to RichTextField.tsx

--- src/app/components/RichTextField.tsx
+++ src/app/components/RichTextField.tsx
@@ -10,18 +10,33 @@ const ReactQuill = dynamic(() => import("react-quill"), {
   loading: () => <p>Loading editor...</p>,
 });
 
+/**  Defines the parameters needed to generate a React component with richtext. */
 export interface RichTextFieldProps {
+  /** name of the richtext property */
   name: string;
   wikiDataProp?: string;
+  /**
+   * Method that is called when the richtext changes
+   * @param name name of the richtextproperty
+   * @param content content of the richtext field
+   */
   updateContent: (name: string, content: string) => void;
 }
 
+/**
+ * React component for generating a richtextfield
+ * @param param0 Object with the parameters for the richtextfield
+ */
 const RichTextField: React.FC<RichTextFieldProps> = ({
   name,
   updateContent,
 }) => {
   const [content, setContent] = useState("");
 
+  /**
+   * Method for handling changes of the richtext
+   * @param content Richtext
+   */
   const handleChange = (content: string) => {
     setContent(content);
     updateContent(name, content); // Update parent component's state
